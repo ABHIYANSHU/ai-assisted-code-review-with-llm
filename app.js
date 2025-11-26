a132a350f77f4cee22de904e0ed34216b9e3967e
Abhinav Srivastava <abhinav.srivastava5@epsilon.com>
2025-11-26 15:27:00 +0530

    fixing issue

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-// app.js - The "Vulnerable" Code
 const express = require('express');
 const app = express();
 
-// 1. Hardcoded Secret (Security Risk)
-const DB_PASSWORD = "SuperSecretPassword1234!"; 
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 app.get('/login', (req, res) => {
     const user = req.query.user;
-    
-    // 2. Dangerous Eval (Security Risk)
-    // This allows remote code execution!
-    eval("console.log('User logged in: " + user + "')");
 
-    res.send("Logged in");
+    console.log(`User logged in: ${user}`);
+
+    res.send("Logged In");
 });
 
 app.listen(3000);
